67d8c6e32b090cf368c792fe297ab97743d60ecc
amirkogit <amirkomail@gmail.com>
2016-12-02 11:25:54 -0500

    method overloading demo

--- LearnCSharp/Grades/Program.cs
+++ LearnCSharp/Grades/Program.cs
@@ -13,14 +13,27 @@ namespace Grades
             Console.WriteLine("Grades project");
 
             GradeBook book = new GradeBook();
+            book.Name = "First Gradebook";
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
 
             GradeStatistics stats = book.ComputeStatistics();
-            Console.WriteLine(stats.AverageGrade);
-            Console.WriteLine(stats.HighestGrade);
-            Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(book.Name);
+
+            WriteResult("Average", stats.AverageGrade);
+            WriteResult("Highest", (int)stats.HighestGrade);
+            WriteResult("Lowest", stats.LowestGrade);
+        }
+
+        static void WriteResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+        static void WriteResult(string description, float result)
+        {
+            Console.WriteLine($"{description} : {result:F2}", description, result);
         }
     }
 }
