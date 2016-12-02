c559d86675968d5cd17dcb4bae78953dfb17e7af
amirkogit <amirkomail@gmail.com>
2016-12-02 09:56:21 -0500

    Added classes to "Grades" project

--- LearnCSharp/Grades/Program.cs
+++ LearnCSharp/Grades/Program.cs
@@ -11,6 +11,16 @@ namespace Grades
         static void Main(string[] args)
         {
             Console.WriteLine("Grades project");
+
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
         }
     }
 }
