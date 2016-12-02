960f445239e3190f21694f706c3a41b8fb7d460e
amirkogit <amirkomail@gmail.com>
2016-12-02 11:07:22 -0500

    Added unit test class for random testing

---
+++ LearnCSharp/Grades.Tests/Types/TypeTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades.Tests.Types
+{
+    [TestClass]
+    public class TypeTests
+    {
+        [TestMethod]
+        public void UsingArrays()
+        {
+            float[] grades;
+            grades = new float[3];
+
+            AddGrades(grades);
+
+            Assert.AreEqual(89.1f, grades[1]);
+        }
+
+        // helper function to add grades
+        private void AddGrades(float[] grades)
+        {
+            grades[1] = 89.1f;
+        }
+
+        [TestMethod]
+        public void UppercaseString()
+        {
+            string name = "hello";
+            name = name.ToUpper();
+            Assert.AreEqual("HELLO", name);
+        }
+
+        [TestMethod]
+        public void AddDaysToDateTime()
+        {
+            DateTime date = new DateTime(2016, 12, 2);
+            date = date.AddDays(1);
+            Assert.AreEqual(3, date.Day);
+        }
+
+        [TestMethod]
+        public void ValueTypesPassByValue()
+        {
+            int x = 46;
+            IncrementNumber(ref x);
+            Assert.AreEqual(47, x);
+
+        }
+
+        // helper function
+        private void IncrementNumber(ref int number)
+        {
+            number += 1;
+        }
+
+        [TestMethod]
+        public void ReferenceTypesPassByValue()
+        {
+            GradeBook book1 = new GradeBook();
+            book1.Name = "Gradebook 1";
+            GradeBook book2 = book1;
+
+            GiveBookAName(ref book2);
+            Assert.AreEqual("A gradebook", book2.Name);
+        }
+
+        private void GiveBookAName(ref GradeBook book)
+        {
+            book = new GradeBook();
+            book.Name = "A gradebook";
+        }
+
+        [TestMethod]
+        public void GradeBookVariablesHoldAReference()
+        {
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+
+            g1.Name = "Gradebook 1";
+            Assert.AreEqual(g1.Name, g2.Name);
+        }
+    }
+}
