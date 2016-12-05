3ad25c3fb54891408732d6c7dfa30fdf6cc62a76
amirkogit <amirkomail@gmail.com>
2016-12-05 14:11:30 -0500

    OOP flavors: Implementing interfaces, abstract classes, inheritance and polymorphism

--- LearnCSharp/Grades/Program.cs
+++ LearnCSharp/Grades/Program.cs
@@ -13,7 +13,7 @@ namespace Grades
         {
             //OldMain();
 
-            GradeBook book = new GradeBook();
+            IGradeTracker book = CreateGradeBook();
 
             GetBookName(book);
             AddGrades(book);
@@ -21,8 +21,12 @@ namespace Grades
             WriteResults(book);
         }
 
+        private static IGradeTracker CreateGradeBook()
+        {
+            return new ThrowAwayGradeBook();
+        }
 
-        private static void GetBookName(GradeBook book)
+        private static void GetBookName(IGradeTracker book)
         {
             try
             {
@@ -36,14 +40,14 @@ namespace Grades
             }
         }
 
-        private static void AddGrades(GradeBook book)
+        private static void AddGrades(IGradeTracker book)
         {
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
         }
 
-        private static void SaveGrades(GradeBook book)
+        private static void SaveGrades(IGradeTracker book)
         {
             using (StreamWriter outputFile = File.CreateText("grades.txt"))
             {
@@ -51,13 +55,19 @@ namespace Grades
             }
         }
 
-        private static void WriteResults(GradeBook book)
+        private static void WriteResults(IGradeTracker book)
         {
             GradeStatistics stats = book.ComputeStatistics();
+
+            foreach(float grade in book)
+            {
+                Console.WriteLine(grade);
+            }
+
             WriteResult("Average", stats.AverageGrade);
             WriteResult("Highest", (int)stats.HighestGrade);
             WriteResult("Lowest", stats.LowestGrade);
-
+            WriteResult(stats.Description, stats.LetterGrade);
         }
 
         private static void OldMain()
