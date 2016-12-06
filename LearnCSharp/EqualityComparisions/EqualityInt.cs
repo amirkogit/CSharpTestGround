52aac0450c7cbbe008c39194bb2f8c4385187e8c
amirkogit <amirkomail@gmail.com>
2016-12-06 15:11:55 -0500

    Peeking into IL Diassembler for EqualityInt checks
    - note the line with "call" in case of method version, and the line with
    "ceq" for == operator version

--- LearnCSharp/EqualityComparisions/EqualityInt.cs
+++ LearnCSharp/EqualityComparisions/EqualityInt.cs
@@ -19,9 +19,37 @@ namespace EqualityComparisions
             return x == y; // returns TRUE : evaluates value equality
         }
 
+        // Peeking into IL Diassembler
+        // ------------------------------------------------------------
+
+        //.method private hidebysig static bool AreIntsEqualOp(int32 x,
+        //                                                int32 y) cil managed
+        //{
+        //// Code size       5 (0x5)
+        //.maxstack  8
+        //IL_0000:  ldarg.0
+        //IL_0001:  ldarg.1
+        //IL_0002:  ceq
+        //IL_0004:  ret
+        //} // end of method EqualityInt::AreIntsEqualOp
+
         static bool AreIntsEqualMethod(int x, int y)
         {
             return x.Equals(y); // returns TRUE : evaluates value equality
         }
+
+        // Peeking into IL Diassembler
+        // ------------------------------------------------------------
+
+        //.method private hidebysig static bool AreIntsEqualMethod(int32 x,
+        //                                            int32 y) cil managed
+        //{
+        //// Code size       9 (0x9)
+        //.maxstack  8
+        //IL_0000:  ldarga.s x
+        //IL_0002:  ldarg.1
+        //IL_0003:  call instance bool[mscorlib] System.Int32::Equals(int32)
+        //IL_0008:  ret
+        //} // end of method EqualityInt::AreIntsEqualMethod
     }
 }
