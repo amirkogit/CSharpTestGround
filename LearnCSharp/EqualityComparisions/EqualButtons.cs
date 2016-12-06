8dc51c6e8028af51c78c34eec7082a25fe6111d8
amirkogit <amirkomail@gmail.com>
2016-12-06 09:55:23 -0500

    Added a new project for learning equality and comparisions in C#

---
+++ LearnCSharp/EqualityComparisions/EqualButtons.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EqualityComparisions
+{
+    public class EqualButtons
+    {
+        public static void RunDemo()
+        {
+            Console.WriteLine("Check if two buttons are equal.");
+
+            Button button1 = new Button();
+            button1.Text = "Button 1";
+
+            Button button2 = new Button();
+            button2.Text = "Button 2";
+
+            Console.WriteLine(button1 == button2);
+        }
+    }
+}
---
+++ LearnCSharp/EqualityComparisions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualityComparisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Equality-Comparisions demo.\n");
+
+            EqualButtons.RunDemo();
+        }
+    }
+}
