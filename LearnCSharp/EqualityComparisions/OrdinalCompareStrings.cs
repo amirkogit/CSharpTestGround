ba444b23e446a24554939c76ada1519b87142fa6
amirkogit <amirkomail@gmail.com>
2016-12-08 09:46:13 -0500

    minor cosmetics

--- LearnCSharp/EqualityComparisions/OrdinalCompareStrings.cs
+++ LearnCSharp/EqualityComparisions/OrdinalCompareStrings.cs
@@ -5,10 +5,14 @@ namespace EqualityComparisons
     {
         public static void RunDemo()
         {
+            Console.WriteLine("OrdinalCompareStrings demo:");
+
             string str1 = "computer";
             string str2 = "COMPUTER";
             int result = string.Compare(str1, str2, StringComparison.Ordinal);
             Console.WriteLine($"Compare result for {str1} and {str2}: " + result);
+
+            Console.WriteLine();
         }
     }
 }
