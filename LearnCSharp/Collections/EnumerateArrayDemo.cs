f4e260739f5c53340ce58c21018eae21fdc9bd32
amirkogit <amirkomail@gmail.com>
2016-12-14 15:06:04 -0500

    Created new project to test out different collection classes

---
+++ LearnCSharp/Collections/EnumerateArrayDemo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class EnumerateArrayDemo
+    {
+        public static void RunDemo()
+        {
+            Console.WriteLine("EnumerateArrayDemo: Compares using for and foreach to enumerate arrays");
+
+            string[] daysofWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            Console.WriteLine("Enumerating using foreach");
+            foreach(var day in daysofWeek)
+                Console.WriteLine(day);
+
+            Console.WriteLine();
+            Console.WriteLine("Enumerating using for loop");
+            for(int i = 0; i < daysofWeek.Length; i++)
+            {
+                Console.WriteLine(daysofWeek[i]);
+            }
+        }
+    }
+}
